eeee6ec0164814cb5ab13d1329c29baf81cdeead
Corintho Assunção <corintho.filho@gmail.com>
2024-05-08 15:43:16 +0200

    feat: add overseer

--- lua/community.lua
+++ lua/community.lua
@@ -10,6 +10,7 @@ return {
   { import = "astrocommunity.pack.typescript" },
   -- Coding
   { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
+  { import = "astrocommunity.code-runner.overseer-nvim" },
   { import = "astrocommunity.completion.copilot-lua" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.editing-support.nvim-treesitter-context" },
