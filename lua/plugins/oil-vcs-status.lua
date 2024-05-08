a526d66b573364e4fbc8f88c709db769e4f43ea2
Corintho Assunção <corintho.filho@gmail.com>
2024-05-08 14:11:27 +0200

    fix(oil): proper setup for oil vcs status

--- lua/plugins/oil-vcs-status.lua
+++ lua/plugins/oil-vcs-status.lua
@@ -4,6 +4,45 @@ return {
     "SirZenith/oil-vcs-status",
     event = "VeryLazy",
     opts = {},
-    dependencies = { "stevearc/oil.nvim" },
+    config = function()
+      local status_const = require "oil-vcs-status.constant.status"
+
+      local StatusType = status_const.StatusType
+
+      require("oil-vcs-status").setup {
+        status_symbol = {
+          [StatusType.Added] = "",
+          [StatusType.Copied] = "󰆏",
+          [StatusType.Deleted] = "",
+          [StatusType.Ignored] = "",
+          [StatusType.Modified] = "",
+          [StatusType.Renamed] = "",
+          [StatusType.TypeChanged] = "󰉺",
+          [StatusType.Unmodified] = " ",
+          [StatusType.Unmerged] = "",
+          [StatusType.Untracked] = "",
+          [StatusType.External] = "",
+
+          [StatusType.UpstreamAdded] = "󰈞",
+          [StatusType.UpstreamCopied] = "󰈢",
+          [StatusType.UpstreamDeleted] = "",
+          [StatusType.UpstreamIgnored] = " ",
+          [StatusType.UpstreamModified] = "󰏫",
+          [StatusType.UpstreamRenamed] = "",
+          [StatusType.UpstreamTypeChanged] = "󱧶",
+          [StatusType.UpstreamUnmodified] = " ",
+          [StatusType.UpstreamUnmerged] = "",
+          [StatusType.UpstreamUntracked] = " ",
+          [StatusType.UpstreamExternal] = "",
+        },
+      }
+    end,
+    dependencies = {
+      { "stevearc/oil.nvim", opts = {
+        win_options = {
+          signcolumn = "yes",
+        },
+      } },
+    },
   },
 }
