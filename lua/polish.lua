c417c8e539c54b8dac05feef3b76fc07e59e1445
Corintho Assunção <corintho.filho@gmail.com>
2024-06-03 13:39:25 +0200

    feat(neovide): add support for neovide

--- lua/polish.lua
+++ lua/polish.lua
@@ -14,3 +14,20 @@ vim.filetype.add {
   --   ["~/%.config/foo/.*"] = "fooscript",
   -- },
 }
+-- Setup for neovide
+if vim.g.neovide then
+  vim.o.guifont = "JetBrainsMono Nerd Font Mono:h16"
+  vim.g.neovide_cursor_animation_length = 0
+
+  -- Proper setup for OSX
+  local jit = require "jit"
+  if jit.os == "OSX" then
+    vim.g.neovide_input_macos_option_key_is_meta = true
+    vim.g.neovide_input_use_logo = 1
+    -- Fixes copy & paste using Cmd on OSX
+    vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+  end
+end
