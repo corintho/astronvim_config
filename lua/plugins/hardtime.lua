9386811638a484eb12dfbc8105e660e62966c7f0
Corintho Assunção <corintho.filho@gmail.com>
2024-10-21 11:04:58 +0200

    chore: remove arrow keys from disabled keys
    now they are part of my home row

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -5,6 +5,7 @@ return {
     opts = function()
       local config = require("hardtime.config").config
       table.insert(config.disabled_filetypes, 1, "Overseer*")
+      config.disabled_keys = {}
     end,
     -- opts = {
     --   disabled_filetypes = {
