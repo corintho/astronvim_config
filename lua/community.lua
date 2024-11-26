a79c73315aea1f1fa463da69c578a4f709655fbd
Corintho Assunção <corintho.filho@gmail.com>
2024-11-26 16:47:55 +0100

    feat: add svelte support

--- lua/community.lua
+++ lua/community.lua
@@ -9,6 +9,7 @@ return {
   { import = "astrocommunity.pack.dart" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.svelte" },
   { import = "astrocommunity.pack.typescript" },
   -- Coding
   { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
