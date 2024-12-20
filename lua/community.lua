07403e4abf18abfad31416a99dfb407fa274a992
Corintho Assunção <corintho.filho@gmail.com>
2024-12-20 09:46:05 +0100

    feat: enable kanagawa theme

--- lua/community.lua
+++ lua/community.lua
@@ -29,6 +29,7 @@ return {
   { import = "astrocommunity.git.neogit" },
   { import = "astrocommunity.git.diffview-nvim" },
   -- Colorschemes
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
   { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
   -- Other
   { import = "astrocommunity.recipes.neovide" },
