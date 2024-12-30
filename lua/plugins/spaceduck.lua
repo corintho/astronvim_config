221202efbc2b3eb5d73396dfe618f45b62021fe7
Corintho Assunção <corintho.filho@gmail.com>
2024-12-30 14:32:44 +0100

    chore: minor adjustment to spaceduck theme

--- lua/plugins/spaceduck.lua
+++ lua/plugins/spaceduck.lua
@@ -10,7 +10,6 @@ return {
         opts = {
           highlights = {
             spaceduck = { -- a table of overrides/changes when applying the astrotheme theme
-              StatusLine = { link = "StatusLineNC" },
               FlashLabel = { link = "EyelinerPrimary" },
             },
           },
